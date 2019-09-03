3649446c4240b47e4f7f25cfca614247724ad8b1
maria-mendoza <54276089+maria-mendoza@users.noreply.github.com>
2019-09-02 23:28:52 -0500

    Ejercicio 3 listo

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,6 +1,12 @@
-
+#include <iostream>
+using namespace std;
 
 int main() {
-
-    return 0;
+    int numero, max=0;
+    do{
+        cout<<"ingrese el numero:";
+        cin<<numero;
+        if(numero>max) max=numero;
+    }while(numero!=0);
+    cout<<"el mayor numero es:"<<max<<endl;
 }
