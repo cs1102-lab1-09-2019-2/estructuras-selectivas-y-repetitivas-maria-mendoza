9465a19778fbde12a7b8b8e22ae262e02e007d65
maria-mendoza <54276089+maria-mendoza@users.noreply.github.com>
2019-09-03 22:46:34 -0500

    Ejercicio 6 listo

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+  double med;
+  int n;
+  cout<<"Ingresar n=";
+  cin>>med;
+  for (int i=0;i<=(med-1);i++){
+    for (int i=0;i<=(med-1);i+=1){
+      n+=1;
+      if((n%2)==0){
+        cout<<"@";
+      }else{
+        cout<<"#";
+      }
+    }
+    cout<<"\n";
+  }
 
-
-int main() {
-
-    return 0;
 }
